26b62f7b0f10b8dc2f04d8fd537720927b9db17f
MaxwellJalves <maxwell880@gmail.com>
2021-11-23 01:00:30 -0300

    refatorado a função para e aplicado no card de contato

--- front-end/src/uteis/conversor.tsx
+++ front-end/src/uteis/conversor.tsx
@@ -1,14 +1,10 @@
-type ISeparator ={
-  espaco: ' ',
-  underline: '_',
-  hifen: '-'
 
-}
 /** @description
  *  Função recebe um texto e um dos separadores previstos e retorna uma string com a formatação camelCase
  */
-export const conversorCamelCase = (texto : string , separator : ISeparator) =>{
-  return texto.split(separator.espaco)
+export const conversorCamelCase = (texto : string , separador : ' ') =>{
+
+  return texto.split(separador)
               .map((t) => t[0].toUpperCase() +  t.slice(1).toLowerCase())
-              .join(separator.espaco)
+              .join(separador)
 }
